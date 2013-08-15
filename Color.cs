6af8d4d0488e369d45d33e89d0411da1d3d38336
Ashiq A <alibhai.ashiq@gmail.com>
2013-08-15 16:54:07 -0400

    Initial commit of base MainConsole class. It can draw stuff, in colour!

---
+++ Color.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleX
+{
+    public class Color
+    {
+        private short cursesColor;
+        private Boolean useBold;
+
+        public static Color Black = new Color(1, false);
+        public static Color DarkBlue = new Color(2, false);
+        public static Color DarkGreen = new Color(3, false);
+        public static Color DarkCyan = new Color(4, false);
+        public static Color DarkRed = new Color(5, false);
+        public static Color DarkPurple = new Color(6, false);
+        public static Color DarkYellow = new Color(7, false);
+        public static Color Grey = new Color(8, false);
+        public static Color DarkGrey = new Color(1, true);  // 9 doesn't work, oddly.
+        public static Color Blue = new Color(10, true);
+        public static Color Green = new Color(11, true);
+        public static Color Cyan = new Color(12, true);
+        public static Color Red = new Color(13, true);
+        public static Color Purple = new Color(14, true);
+        public static Color Yellow = new Color(15, true);
+        public static Color White = new Color(16, true);
+
+        public Color(short cursesColor, bool useBold)
+        {
+            this.cursesColor = cursesColor;
+            this.useBold = useBold;
+        }
+
+        public static implicit operator short(Color c)
+        {
+            return c.cursesColor;
+        }
+
+        public int CursesColor { get { return this.cursesColor; } }
+        public bool UseBold { get { return this.useBold; } }
+    }
+}
---
+++ MainConsole.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CursesSharp;
+
+namespace ConsoleX
+{
+    public class MainConsole : IDisposable
+    {
+        private Window currentWindow;
+        private Color currentColor;
+
+        private static MainConsole instance = new MainConsole();
+
+        public static MainConsole Instance { get { return instance; } }
+
+        private MainConsole()
+        {
+            Curses.InitScr();
+            Curses.StartColor();
+            
+            Curses.InitPair(1, Colors.BLACK, Colors.BLACK);
+            Curses.InitPair(2, Colors.BLUE, Colors.BLACK);
+            Curses.InitPair(3, Colors.GREEN, Colors.BLACK);
+            Curses.InitPair(4, Colors.CYAN, Colors.BLACK);
+            Curses.InitPair(5, Colors.RED, Colors.BLACK);
+            Curses.InitPair(6, Colors.MAGENTA, Colors.BLACK);
+            Curses.InitPair(7, Colors.YELLOW, Colors.BLACK);
+            Curses.InitPair(8, Colors.WHITE, Colors.BLACK);
+            Curses.InitPair(9, Colors.WHITE, Colors.BLACK);
+            Curses.InitPair(10, Colors.BLUE, Colors.BLACK);
+            Curses.InitPair(11, Colors.GREEN, Colors.BLACK);
+            Curses.InitPair(12, Colors.CYAN, Colors.BLACK);
+            Curses.InitPair(13, Colors.RED, Colors.BLACK);
+            Curses.InitPair(14, Colors.MAGENTA, Colors.BLACK);
+            Curses.InitPair(15, Colors.YELLOW, Colors.BLACK);
+            Curses.InitPair(16, Colors.WHITE, Colors.BLACK);            
+
+            this.currentWindow = Curses.StdScr;
+        }
+
+        public void Clear()
+        {
+            this.currentWindow.Clear();
+        }
+
+        public void Dispose()
+        {
+            Curses.EndWin();
+        }
+
+        public void Refresh()
+        {
+            this.currentWindow.Refresh();
+        }
+
+        public void Write(int x, int y, string text)
+        {
+            if (x == 79 && y == 24)
+            {
+                // There's a bug in CursesSharp. This sucks down our performance, but, I dunno.
+                // Alternatively, just always use Box() and hide this character, or don't draw it.
+                // The exception occurs, but the character still draws.
+                try
+                {
+                    this.currentWindow.Add(y, x, text);
+                }
+                catch
+                {
+                }
+            }
+            else
+            {
+                // Black doesn't show
+                if (this.currentColor != Color.Black)
+                {
+                    this.currentWindow.Add(y, x, text);
+                }
+            }
+        }
+
+        public Color Color { set {
+
+            currentColor = value;
+            this.currentWindow.Color = (short)currentColor;
+            var colorAsShort = (short)currentColor;
+
+            if (currentColor.UseBold)
+            {
+                this.currentWindow.AttrOn(CursesSharp.Attrs.BOLD);
+            }
+            else
+            {
+                this.currentWindow.AttrOff(CursesSharp.Attrs.BOLD);
+            }
+        } }
+    }
+}
